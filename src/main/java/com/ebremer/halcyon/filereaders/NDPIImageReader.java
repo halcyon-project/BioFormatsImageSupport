a84241d67d83262190242a8fda83de64b75abee8
Erich Bremer <erich@ebremer.com>
2023-08-28 14:52:46 -0400

    first commit

---
+++ src/main/java/com/ebremer/halcyon/filereaders/NDPIImageReader.java
@@ -0,0 +1,94 @@
+package com.ebremer.halcyon.filereaders;
+
+import com.ebremer.halcyon.lib.ImageMeta;
+import com.ebremer.halcyon.lib.ImageRegion;
+import com.ebremer.halcyon.lib.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import loci.formats.FormatException;
+import loci.formats.gui.BufferedImageReader;
+import loci.formats.in.NDPIReader;
+import org.apache.jena.rdf.model.Model;
+
+/**
+ *
+ * @author erich
+ */
+public class NDPIImageReader extends AbstractImageReader {
+    private BufferedImageReader reader;
+    private final ImageMeta meta;
+    
+    public NDPIImageReader(URI uri) throws IOException {
+        reader = new BufferedImageReader(new NDPIReader());
+        File file = new File(uri);
+        try {
+            reader.setId(file.toString());
+        } catch (FormatException ex) {
+            Logger.getLogger(NDPIImageReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        reader.setSeries(0);
+        ImageMeta.Builder builder = ImageMeta.Builder.getBuilder(0, reader.getSizeX(), reader.getSizeY())
+                .setTileSizeX(reader.getOptimalTileWidth())
+                .setTileSizeY(reader.getOptimalTileHeight());
+        for(int i=1; i<reader.getSeriesCount();i++) {
+            reader.setSeries(i);
+            builder.addScale(i, reader.getSizeX(), reader.getSizeY());
+        }
+        meta = builder.build();
+    }
+
+    @Override
+    public String getFormat() {
+        return "ndpi";
+    }
+
+    private BufferedImage readTile(ImageRegion region, int series) {
+        reader.setSeries(series);
+        try {
+            return reader.openImage(0, region.getX(), region.getY(), region.getWidth(), region.getHeight());
+        } catch (FormatException ex) {
+            Logger.getLogger(NDPIImageReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NDPIImageReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NDPIImageReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public BufferedImage readTile(ImageRegion region, Rectangle preferredsize) {
+        ImageMeta.ImageScale scale = meta.getBestMatch(Math.max((double) region.getWidth()/(double) preferredsize.width(),(double) region.getHeight()/ (double) preferredsize.height()));
+        return readTile(scale.Validate(region.scaleRegion(scale.scale())),scale.series());
+    }
+    
+    @Override
+    public ImageMeta getImageMeta() {
+        return meta;
+    }
+
+    @Override
+    public Model getMeta() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public Set<String> getSupportedFormats() {
+        Set<String> set = new HashSet<>();
+        set.add("ndpi");
+        return set;
+    }
+}
